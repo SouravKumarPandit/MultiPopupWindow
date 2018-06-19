23578c8df2e7b092849513f6b55b358e903121b9
sourav kumar <soupandit@gmail.com>
2018-06-19 11:30:54 +0530

    Update PopUpLinearLayout.java

--- app/src/main/java/com/focus/sourav/multipopupwindow/PopUpLinearLayout.java
+++ app/src/main/java/com/focus/sourav/multipopupwindow/PopUpLinearLayout.java
@@ -117,6 +117,116 @@ public class PopUpLinearLayout extends LinearLayout {
         if (rx > width / 2) rx = width / 2;
         if (ry > height / 2) ry = height / 2;
         float widthMinusCorners = (width - (2 * rx));
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
+import android.widget.LinearLayout;
+
+public class PopUpLinearLayout extends LinearLayout {
+
+    private Paint bgPaint;
+    Path baloonPath;
+    Path trangelPath;
+    Context mContext;
+    int bgColor = 0;
+
+    int[] colorValArr = new int[]{0xffd3deff, 0xffd3fff7, 0xffd3ffd5, 0xffffecd3, 0xffead3ff, 0xfffffddb};
+
+
+    public void setDrawableRight(boolean drawableRight) {
+        isDrawableRight = drawableRight;
+        init(mContext);
+        invalidate();
+    }
+
+    private boolean isDrawableRight;
+
+    public PopUpLinearLayout(Context context) {
+        super(context);
+        mContext = context;
+        init(context);
+
+    }
+
+    public void setBgPaintColor(int bgColor) {
+        this.bgColor = bgColor;
+        init(mContext);
+        invalidate();
+    }
+
+    public PopUpLinearLayout(Context context, @Nullable AttributeSet attrs) {
+
+        super(context, attrs);
+        mContext = context;
+        init(context);
+
+    }
+
+    private void init(Context context) {
+        this.setOrientation(LinearLayout.VERTICAL);
+        this.setGravity(Gravity.CENTER);
+        int leftMargin;
+        int rightMargin = (int) dpToPixel(5);
+        if (isDrawableRight) {
+            leftMargin = (int) dpToPixel(5);
+            rightMargin = (int) dpToPixel(15);
+        } else {
+            leftMargin = (int) dpToPixel(15);
+            rightMargin = (int) dpToPixel(5);
+
+        }
+
+        int topMargin = (int) dpToPixel(5);
+        int bottomMargin = (int) dpToPixel(10);
+        setPadding(leftMargin, topMargin, rightMargin, bottomMargin);
+
+        bgPaint = new Paint();
+        bgPaint.setStrokeCap(Paint.Cap.ROUND);
+        bgPaint.setStyle(Paint.Style.FILL);
+        bgPaint.setColor(colorValArr[bgColor]);
+        bgPaint.setAntiAlias(true);
+
+        baloonPath = new Path();
+        trangelPath = new Path();
+
+
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+
+        getTickerPath(dpToPixel(12), dpToPixel(50), canvas);
+        canvas.drawPath(trangelPath, bgPaint);
+        if (isDrawableRight)
+            roundedRect(0, 0, canvas.getWidth() - dpToPixel(10), canvas.getHeight(), dpToPixel(15), dpToPixel(15), false);
+        else
+            roundedRect(dpToPixel(10), 0, canvas.getWidth(), canvas.getHeight(), dpToPixel(15), dpToPixel(15), false);
+
+        canvas.drawPath(baloonPath, bgPaint);
+//        bgPaint.setColor(Color.RED);
+        super.dispatchDraw(canvas);
+        super.dispatchDraw(canvas);
+    }
+
+    public void roundedRect(float left, float top, float right, float bottom, float rx, float ry, boolean conformToOriginalPost) {
+//        Path path = new Path();
+        if (rx < 0) rx = 0;
+        if (ry < 0) ry = 0;
+        float width = right - left;
+        float height = bottom - top;
+        if (rx > width / 2) rx = width / 2;
+        if (ry > height / 2) ry = height / 2;
+        float widthMinusCorners = (width - (2 * rx));
         float heightMinusCorners = (height - (2 * ry));
 
         baloonPath.moveTo(right, top + ry);
@@ -125,14 +235,11 @@ public class PopUpLinearLayout extends LinearLayout {
         baloonPath.rQuadTo(-rx, 0, -rx, ry); //top-left corner
         baloonPath.rLineTo(0, heightMinusCorners);
 
-        if (conformToOriginalPost)
-        {
+        if (conformToOriginalPost) {
             baloonPath.rLineTo(0, ry);
             baloonPath.rLineTo(width, 0);
             baloonPath.rLineTo(0, -ry);
-        }
-        else
-        {
+        } else {
             baloonPath.rQuadTo(0, ry, rx, ry);//bottom-left corner
             baloonPath.rLineTo(widthMinusCorners, 0);
             baloonPath.rQuadTo(rx, 0, rx, -ry); //bottom-right corner
@@ -146,33 +253,27 @@ public class PopUpLinearLayout extends LinearLayout {
     }
 
 
-    public void setGradient(int sColor, int eColor)
-    {
+    public void setGradient(int sColor, int eColor) {
         bgPaint.setShader(new LinearGradient(0, 0, 0, getHeight() / 2,
                 new int[]{sColor, eColor},
                 new float[]{0.0f, 1.0f}, Shader.TileMode.MIRROR));
     }
 
-    public static int getScreenWidth()
-    {
+    public static int getScreenWidth() {
         return Resources.getSystem().getDisplayMetrics().widthPixels;
     }
 
-    public static int getScreenHeight()
-    {
+    public static int getScreenHeight() {
         return Resources.getSystem().getDisplayMetrics().heightPixels;
     }
 
-    public static float dpToPixel(float dp)
-    {
+    public static float dpToPixel(float dp) {
         DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
         return dp * metrics.density;
     }
 
-    public void getTickerPath(float positionX, float positionY, Canvas canvas)
-    {
-        if (isDrawableRight)
-        {
+    public void getTickerPath(float positionX, float positionY, Canvas canvas) {
+        if (isDrawableRight) {
             trangelPath.reset();
             trangelPath.moveTo(canvas.getWidth(), dpToPixel(35));
             trangelPath.lineTo(canvas.getWidth() - dpToPixel(12), dpToPixel(25));
@@ -180,9 +281,7 @@ public class PopUpLinearLayout extends LinearLayout {
             trangelPath.lineTo(canvas.getWidth(), dpToPixel(35));
             trangelPath.close();
 
-        }
-        else
-        {
+        } else {
             trangelPath.reset();
             trangelPath.moveTo(0, dpToPixel(35));
             trangelPath.lineTo(dpToPixel(12), dpToPixel(25));
@@ -194,3 +293,4 @@ public class PopUpLinearLayout extends LinearLayout {
     }
 
 }
+
